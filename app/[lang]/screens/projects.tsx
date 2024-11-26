d0de25790af27a228579ed294e79efeb20dc1624
RAZRULETEL <qwerty.qwertlain@gmail.com>
2024-11-27 02:03:17 +0300

    Add vk post link for FooDate project

--- app/[lang]/screens/projects.tsx
+++ app/[lang]/screens/projects.tsx
@@ -14,13 +14,19 @@ export default function ProjectsScreen({dict}: { dict: { [key: string]: string }
 			<ProjectView img={aitip_main.src} title={dict.aitip} accentColor='blue'>
 				<p className={styles.projects__text}>{dict.aitip_about}</p>
 				<div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
-					<a href='https://github.com/Intention-man/new_aitip_site' className={styles.projects__visit}>GitHub →</a>
-					<a href='https://vk.com/wall-115604474_1397' className={styles.projects__visit}>{dict.post_in} VK →</a>
+					<a href='https://github.com/Intention-man/new_aitip_site'
+					   className={styles.projects__visit}>GitHub →</a>
+					<a href='https://vk.com/wall-115604474_1397'
+					   className={styles.projects__visit}>{dict.post_in} VK →</a>
 				</div>
 			</ProjectView>
 			<ProjectView img={foodate_main.src} title={dict.foodate} direction={'row-reverse'} accentColor='#269926'>
 				<p className={styles.projects__text}>{dict.foodate_about}</p>
-				<a href='https://foodate.ru' className={styles.projects__visit}>{dict.visit + ' →'}</a>
+				<div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+					<a href='https://vk.com/wall-213504991_17'
+					   className={styles.projects__visit}>{dict.post_in} VK →</a>
+					<a href='https://foodate.ru' className={styles.projects__visit}>{dict.visit + ' →'}</a>
+				</div>
 			</ProjectView>
 		</section>
 	)

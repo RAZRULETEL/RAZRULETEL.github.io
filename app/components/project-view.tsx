b916254c6e84ed103ea4ac6643e01e1a1c6d8636
RAZRULETEL <qwerty.qwertlain@gmail.com>
2025-05-16 21:52:23 +0300

    Implement project view on click animation, update projects images with full page ones

--- app/components/project-view.tsx
+++ app/components/project-view.tsx
@@ -1,12 +1,33 @@
+'use client'
 import styles from "./project-view.module.css";
-import React from "react";
+import React, {useRef} from "react";
 
 export default function ProjectView({img, children, title, direction = 'row', accentColor}:
-{ img: string, children: React.ReactNode, title: string, direction?: 'row' | 'row-reverse', accentColor?: string }) {
+										{
+											img: string,
+											children: React.ReactNode,
+											title: string,
+											direction?: 'row' | 'row-reverse',
+											accentColor?: string
+										}) {
+	const containerRef = useRef<HTMLDivElement>(null);
 
 	return (
-		<div className={styles.project_view + ' ' + (direction == 'row' ? styles.project_view_reverse : '')} style={{'--accent-color': accentColor || ''} as React.CSSProperties}>
-			<div className={styles.image + ' ' + (direction === 'row-reverse' ? styles.image_reverse : '')}>
+		<div className={styles.project_view
+			+ ' ' + (direction == 'row' ? styles.project_view_reverse : '')}
+			 ref={containerRef}
+			 style={{'--accent-color': accentColor || ''} as React.CSSProperties}>
+			<div className={styles.image
+				+ ' ' + (direction === 'row-reverse' ? styles.image_reverse : '')}
+				 onClick={() => {
+					 if (containerRef.current) {
+						 if (containerRef.current.classList.contains(styles.project_view_preview)) {
+							 containerRef.current.classList.remove(styles.project_view_preview);
+						 } else {
+							 containerRef.current.classList.add(styles.project_view_preview);
+						 }
+					 }
+				 }}>
 				<img src={img} alt=""/>
 			</div>
 			<div className={styles.content}>
